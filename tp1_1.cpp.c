784e28bb9186fe529f4c3544fdf5a359f7063844
leonardo4390 <tidus4390@gmail.com>
2024-05-12 18:30:15 -0300

    segundo commit

--- tp1_1.cpp.c
+++ tp1_1.cpp.c
@@ -3,9 +3,20 @@
 
 int main()
 {
-
+    int numero = 20;
+    int *p_numero;
+    int tama;
 
     printf("hola mundo");
+
+    p_numero = &numero;
+    tama = sizeof(numero);
+
+
+    printf("\nContenido del puntero: %d",*p_numero);
+    printf("\nDireccion de memoria almacenada por el puntero: %p",p_numero);
+    printf("\nLa direccin de memora de la varible: %p",&numero);
+    printf("\nel tamaño de memoria utilizado por la variable : %d",tama);
 
     return 0;
 }
